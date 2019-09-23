ad9f569bc07760dff04bd0dfe8ab03ea7c74485c
Chris Young <beesucker@gmail.com>
2019-09-23 09:21:40 -0700

    generate build factories

--- domain/matches/match/factories/build.rb
+++ domain/matches/match/factories/build.rb
@@ -3,19 +3,31 @@ module SoccerSeason
     module Matches
       class Match
         module Factories
+          # Recursively build domain objects.  Used in the domain as the primary
+          # factory. The results of #build can be passed to any other build
+          # factory in the domain. Takes a hash, or a list of hashes.  If a list
+          # is given, it will return a list of the instantiated objects
           class Build
-            def self.factory(args)
+            def self.factory(args=nil)
+              return args if args.nil?
+              return args unless args.is_a?(Hash)
+
+              [args].flatten.map do |domain_object_args|
+                new_domain_object(domain_object_args)
+              end.tap do |result|
+                return result.first unless args.is_a?(Array)
+              end
+            end
+
+            def self.new_domain_object(args)
               Match.new(
-                id: args[:id],
-                fixture: args[:fixture].is_a?(Hash) ? Fixture.build(args[:fixture]) : args[:fixture],
-                goals: (args[:goals] || []).map do |object|
-                  object.is_a?(Hash) ? Goal.build(object) : object
-                end,
-                result: args[:result].is_a?(Hash) ? Result.build(args[:result]) : args[:result],
-                teams: (args[:teams] || []).map do |object|
-                  object.is_a?(Hash) ? {:Teams=>:Team}.build(object) : object
-                end,
-                pitch: args[:pitch].is_a?(Hash) ? Pitch.build(args[:pitch]) : args[:pitch],
+                args.merge(
+                  fixture: Fixture.build(args[:fixture]),
+                  goals: Goal.build(args[:goals]),
+                  result: Result.build(args[:result]),
+                  teams: Domain::Teams::Team.build(args[:teams]),
+                  pitch: Pitch.build(args[:pitch]),
+                )
               )
             end
           end
